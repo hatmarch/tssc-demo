7b390c1f4703a69aa677811b7fbdb81aa53a6f35
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-03-15 12:16:32 +0000

    Add demo instalation scripts
    Add shell scripts to wrap ansible playbook
    
    Add ansible scaffolding
    
    Convert rekor subrepo yaml into ansible templates
    
    Create ansible playbook for deploying rekor in {{ rekor_project }}

---
+++ scripts/create-demo.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+declare -r SCRIPT_DIR=$(cd -P $(dirname $0) && pwd)
+declare PROJECT_PREFIX="tssc-demo"
+declare -r ANSIBLE_DEMO_PLAYBOOK_DIR="${SCRIPT_DIR}/../ansible"
+declare ANSIBLE_ACTION="create"
+
+display_usage() {
+cat << EOF
+$0: Create CDC Monolith Demo --
+
+  Usage: ${0##*/} [ OPTIONS ]
+  
+    -p <TEXT>  [optional] Project prefix to use.  Defaults to "cdc-demo"
+    -v         [optional] Run ansible playbooks in verbose mode
+    -i         [optional] Install pre-requisites
+
+EOF
+}
+
+get_and_validate_options() {
+  # Transform long options to short ones
+#   for arg in "$@"; do
+#     shift
+#     case "$arg" in
+#       "--long-x") set -- "$@" "-x" ;;
+#       "--long-y") set -- "$@" "-y" ;;
+#       *)        set -- "$@" "$arg"
+#     esac
+#   done
+
+  
+  # parse options
+  while getopts ':vip:h' option; do
+      case "${option}" in
+          p  ) p_flag=true; PROJECT_PREFIX="${OPTARG}";;
+          v  ) ANSIBLE_VERBOSE="-vvvvv";;
+          i  ) ANSIBLE_INSTALL_PRE="-e install_prereq=true";;
+          h  ) display_usage; exit;;
+          \? ) printf "%s\n\n" "  Invalid option: -${OPTARG}" >&2; display_usage >&2; exit 1;;
+          :  ) printf "%s\n\n%s\n\n\n" "  Option -${OPTARG} requires an argument." >&2; display_usage >&2; exit 1;;
+      esac
+  done
+  shift "$((OPTIND - 1))"
+
+  if [[ -z "${PROJECT_PREFIX}" ]]; then
+      printf '%s\n\n' 'ERROR - PROJECT_PREFIX must not be null' >&2
+      display_usage >&2
+      exit 1
+  fi
+}
+
+main() {
+    # import common functions
+    . $SCRIPT_DIR/common-func.sh
+
+    trap 'error' ERR
+    trap 'cleanup' EXIT SIGTERM
+    trap 'interrupt' SIGINT
+
+    get_and_validate_options "$@"
+
+    # Run the ansible playbook (see inventory file for additional variables that can be overriden with -e)
+    echo "Attempting to run the ansible playbook for demo"
+    ansible-playbook -i ${ANSIBLE_DEMO_PLAYBOOK_DIR}/inventory ${ANSIBLE_DEMO_PLAYBOOK_DIR}/main.yaml \
+      -e "ACTION=${ANSIBLE_ACTION}" \
+      $(echo ${ANSIBLE_VERBOSE:-""}) $(echo "${ANSIBLE_INSTALL_PRE:-""""}")
+
+    echo "Demo installation completed without error."
+}
+
+main "$@"
