fe2bfef3766e5a3505b63b56d374f551b95fec2e
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-03-17 03:58:17 +0000

    Update delete script to remove missing role bindings

--- helm/charts/ploigos/delete-tssc.sh
+++ helm/charts/ploigos/delete-tssc.sh
@@ -4,3 +4,5 @@ oc delete tsscplatforms tsscplatform --wait
 helm uninstall ploigos --namespace devsecops
 oc delete all --all -n devsecops
 oc delete project devsecops
+oc delete ClusterRoleBinding crd-reader-binding
+oc delete ClusterRole crd-reader
