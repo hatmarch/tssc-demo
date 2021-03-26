a4d6471d01eadc73592dce299b33911130af9afc
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-03-26 06:31:41 +0000

    Connect to rekor host

--- scripts/shell-setup.sh
+++ scripts/shell-setup.sh
@@ -2,3 +2,13 @@
 
 export DEMO_HOME=$( cd "$(dirname "$0")/.." ; pwd -P )
 export PATH="${PATH}:${DEMO_HOME}/bin"
+
+# attempt to set the server to use for rekor (rekor cli uses viper and automaticenv which means all command line flags such as
+# --rekor_server can be read as REKOR_REKOR_SERVER.  See here: https://github.com/spf13/viper)
+REKOR_HOST="$(oc get route rekor-server -n tssc-demo-rekor -o jsonpath='{.spec.host}')"
+if [[ -n "${REKOR_HOST}" ]]; then
+    echo "Exporting REKOR_SERVER for use with rekor cli commands (rekor host is: ${REKOR_HOST}"
+    export REKOR_REKOR_SERVER="http://${REKOR_HOST}"
+else
+    echo "Could not find REKOR host"
+fi
