139aa16715e5d6d1c506aa1bc26c2ad3bc8de395
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-04-07 13:54:58 +0000

    Add walkthrough documentation for pipeline creation
    Also update deployment configuration
    
    Add images for walkthrough
    
    Remove some of the demonstration content from the template

--- scripts/github-pages-publish.sh
+++ scripts/github-pages-publish.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 declare SITE=${1:-site.yml}
-declare REPO=${2:-https://github.com/hatmarch/cdc-data-monolith}
+declare REPO=${2:-https://github.com/hatmarch/tssc-demo}
 declare BRANCH="gh-pages"
 
 echo "Removing old publish directory"
